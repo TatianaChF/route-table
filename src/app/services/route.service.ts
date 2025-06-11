5820a663748fb9e52a6b1fcc0b7655bdc3f00ded
TatianaChF <tatiana.chefox@gmail.com>
2025-06-11 19:24:47 +0300

    add route service

---
+++ src/app/services/route.service.ts
@@ -0,0 +1,17 @@
+import {Injectable} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {Observable} from 'rxjs';
+import {RouteData} from '../models/route.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class RouteService {
+  private url = '/data.json';
+
+  constructor(private http: HttpClient) {}
+
+  getRoutes(): Observable<RouteData> {
+    return this.http.get<RouteData>(this.url);
+  }
+}
