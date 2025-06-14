ec61b35dd82ea7dfc650519736cf013d58ca148d
TatianaChF <tatiana.chefox@gmail.com>
2025-06-14 15:35:34 +0300

    getting routes and saving them to a variable

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {Route} from './models/route.model';
+import {RouteService} from './services/route.service';
 
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet],
   templateUrl: './app.html',
+  standalone: true,
   styleUrl: './app.css'
 })
-export class App {
-  protected title = 'route-table';
+export class App implements OnInit {
+  routes: Route[] = [];
+
+  constructor(private routeService: RouteService) {}
+
+  ngOnInit() {
+    this.routeService.getRoutes().subscribe(res => {
+      this.routes = res;
+      console.log(this.routes);
+    });
+  }
 }
