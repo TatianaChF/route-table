800b4bef43e584b5fcf2ec7bf1acee5c3e5fdc1f
TatianaChF <tatiana.chefox@gmail.com>
2025-06-15 14:16:38 +0300

    definition of the main data flow

--- src/app/models/route-state.service.ts
+++ src/app/models/route-state.service.ts
@@ -1,8 +1,11 @@
 import {Injectable} from '@angular/core';
+import {BehaviorSubject} from 'rxjs';
+import {Route} from './route.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class RouteStateService {
-
+  private routesSubject = new BehaviorSubject<Route[]>([]);
+  routes$ = this.routesSubject.asObservable();
 }
