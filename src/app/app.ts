2fa7c6b30ae9f85664b2c16396a0bf0f0092ec8c
TatianaChF <tatiana.chefox@gmail.com>
2025-06-15 15:05:52 +0300

    adding subscription variables and unsubscribing from all subscriptions when a component is destroyed

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,9 @@
-import {ChangeDetectorRef, Component, OnInit} from '@angular/core';
+import {ChangeDetectorRef, Component, OnInit, OnDestroy} from '@angular/core';
 import {Route, RouteData} from './models/route.model';
 import {RouteService} from './services/route.service';
 import {CommonModule} from '@angular/common';
 import {RouteStateService} from './services/route-state.service';
-import {finalize} from 'rxjs';
+import {finalize, Subscription} from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -12,17 +12,24 @@ import {finalize} from 'rxjs';
   styleUrl: './app.css',
   imports: [CommonModule]
 })
-export class App implements OnInit {
+export class App implements OnInit, OnDestroy {
   routes: Route[] = [];
+
+  private subscriptions = new Subscription();
 
   constructor(private routeState: RouteStateService, private cdr: ChangeDetectorRef) {}
 
   ngOnInit() {
-    this.routeState.routes$.subscribe(routes => {
+    const routeSub = this.routeState.routes$.subscribe(routes => {
       this.routes = routes;
     });
+    this.subscriptions.add(routeSub);
 
     this.loadRoutes();
+  }
+
+  ngOnDestroy() {
+    this.subscriptions.unsubscribe();
   }
 
   loadRoutes() {
