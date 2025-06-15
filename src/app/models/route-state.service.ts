25f802de417ee5844aa9a28ff8f4fa5dca273588
TatianaChF <tatiana.chefox@gmail.com>
2025-06-15 14:30:18 +0300

    add method to load route and refresh

--- src/app/models/route-state.service.ts
+++ src/app/models/route-state.service.ts
@@ -1,6 +1,7 @@
 import {Injectable} from '@angular/core';
-import {BehaviorSubject} from 'rxjs';
+import {BehaviorSubject, map, Observable, tap} from 'rxjs';
 import {Route} from './route.model';
+import {RouteService} from '../services/route.service';
 
 @Injectable({
   providedIn: 'root'
@@ -8,4 +9,21 @@ import {Route} from './route.model';
 export class RouteStateService {
   private routesSubject = new BehaviorSubject<Route[]>([]);
   routes$ = this.routesSubject.asObservable();
+
+  state$ = this.routes$.pipe();
+
+  constructor(private routeService: RouteService) {}
+
+  loadRoutes(): Observable<Route[]> {
+    return this.routeService.getRoutes().pipe(
+      map(res => res.data),
+      tap(routes => {
+        console.log(routes);
+      })
+    );
+  }
+
+  refresh() {
+    this.loadRoutes().subscribe();
+  }
 }
